2765dd582f4b24c0e78ed5a4fd0581a72e224706
Dmytro Sychevsky <sych@pdffiller.com>
2017-02-03 13:55:06 +0200

    update to es5.5

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -16,7 +16,7 @@ fi
 
 # ECS will report the docker interface without help, so we override that with host's private ip
 AWS_PRIVATE_IP=`curl http://169.254.169.254/latest/meta-data/local-ipv4`
-set -- "$@" --network.publish_host=$AWS_PRIVATE_IP
+set -- "$@" -Enetwork.publish_host=$AWS_PRIVATE_IP
 
 # As argument is not related to elasticsearch,
 # then assume that user wants to run his own process,
